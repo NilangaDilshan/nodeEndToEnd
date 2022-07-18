71181f9e74a14d7dcc370251e832cb048b356cd5
Dilshan Wijetunga <nilanga1990@gmail.com>
2022-07-18 13:16:49 +0800

    Adding promises example

---
+++ SubProjects/asynchronous/promise.js
@@ -0,0 +1,12 @@
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    //resolve(1);
+    reject(new Error("Error!"));
+  }, 2000);
+  //resolve(1); // this is to give back the result to consumer
+  // reject(new Error("message"));  //This is when error occurs and inform the consumer
+});
+
+p.then((result) => console.log("Result: ", result)).catch((error) =>
+  console.log("Error: ", error.message)
+);
