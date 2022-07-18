9cb291ccb8b218db6172ad7851037085fdb4c871
Dilshan Wijetunga <nilanga1990@gmail.com>
2022-07-18 15:56:48 +0800

    Promise enhancements

--- SubProjects/asynchronous/promise.js
+++ SubProjects/asynchronous/promise.js
@@ -1,7 +1,13 @@
+const baseLine = true;
+
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
     //resolve(1);
-    reject(new Error("Error!"));
+    if (baseLine) {
+      resolve("Success!");
+    } else {
+      reject(new Error("Error!"));
+    }
   }, 2000);
   //resolve(1); // this is to give back the result to consumer
   // reject(new Error("message"));  //This is when error occurs and inform the consumer
