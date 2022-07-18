639f3725e2952b45dfa8823557c13960e5e0953e
Dilshan Wijetunga <nilanga1990@gmail.com>
2022-07-18 12:54:20 +0800

    Adding callback example

---
+++ SubProjects/asynchronous/CallbackExample.js
@@ -0,0 +1,36 @@
+/* 
+    CallBack How TO
+ */
+console.log("Before");
+
+/* getUser(1, function (user) {
+  console.log("User: ", user);
+  getRepositories(user.gitHubUserName, displayRepositories);
+}); */
+
+getUser(1, displayUser);
+
+console.log("After");
+
+function displayUser(user) {
+  console.log("User: ", user);
+  getRepositories(user.gitHubUserName, displayRepositories);
+}
+
+function displayRepositories(repos) {
+  //console.log(`Repos: ${repos}`);
+  console.log("Repos: %O", JSON.stringify(repos));
+}
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user from database");
+    callback({ id: id, gitHubUserName: "Mosh" });
+  }, 2000);
+}
+
+function getRepositories(userName, callback) {
+  setTimeout(() => {
+    console.log("Reading GIT Repositories");
+    callback({ userName: userName, repos: ["repo1", "repo2", "repo3"] });
+  }, 2000);
+}
